57606f240e239de21207e978b1d6383bb842b3ce
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-11-08 12:05:09 +0530

    Added : Code for refactor using function.

--- empWageCal.sh
+++ empWageCal.sh
@@ -3,26 +3,52 @@
 echo "Welcome To Employee Wage Computation"
 
 sum=0
-fwage=160
-hwage=80
-nowage=0
 
-echo "Day No     Wage     Total Wage"
+Wage()             <-1 Function>
+{
+if (( $1 == 1 ))
+then
+wage=160
+elif (( $1 == 2 ))
+then
+wage=80
+else
+wage=0
+fi
+}
+
+Attendance()            <-2 Function>
+{
+attend=$((RANDOM%3))
+}
+
+TotalWage()                <-3 Function>
+{
+sum=$(($sum+$1))
+}
+
+echo "Day No      Wage      Total Wage"
 for ((i=1; i<=20; i++))
 do
-attend=$((RANDOM%3))
+Attendance
 case "$attend" in
 1)
-sum=$(( $sum + $fwage ))
-echo "Day $d      "$fwage"    "$sum
+Wage 1
+
+TotalWage $wage
+echo "Day $d       "$wage"      "$sum
 ;;
 2)
-sum=$(( $sum + $hwage ))
-echo "Day $d     "$hwage"     "$sum
+Wage 2
+TotalWage $wage
+
+echo "Day $d       "$wage"       "$sum
 ;;
 *)
-sum=$(( $sum + $hwage ))
-echo "Day $d      "$nowage"      "$sum
+Wage 0
+TotalWage $wage
+
+echo "Day $d       "$wage"        "$sum
 ;;
 esac
 done
